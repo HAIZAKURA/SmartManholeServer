372dbd8143942281c94da5a34601d04f05e24b50
HAIZAKURA <hhbilly99@gmail.com>
2020-04-22 18:46:04 +0800

    simplify permission judgment

--- router/dept.js
+++ router/dept.js
@@ -9,24 +9,16 @@ const Dept = require('../schema/dept')
 
 // dept list get interface
 router.post('/dept', (req, res) => {
-  // user login judgment
-  if (req.session.userinfo) {
-    // user permission judgment
-    if (req.session.userinfo.uauth == 'admin') {
-      // get dept list
-      Dept.find({}, {_id: 0})
-        .then(deptlist => {
-          res.json({
-            status: 'success',
-            dept: deptlist
-          })
+  // user login & permission judgment
+  if (req.session.userinfo.uauth == 'admin') {
+    // get dept list
+    Dept.find({}, {_id: 0})
+      .then(deptlist => {
+        res.json({
+          status: 'success',
+          dept: deptlist
         })
-    } else {
-      res.json({
-        status: 'error',
-        msg: 'unauthorized'
       })
-    }
   } else {
     res.json({
       status: 'error',
@@ -37,30 +29,22 @@ router.post('/dept', (req, res) => {
 
 // dept add interface
 router.post('/dept/add', (req, res) => {
-  // user login judgment
-  if (req.session.userinfo) {
-    // user permission judgment
-    if (req.session.userinfo.uauth == 'admin') {
-      // create dept
-      Dept.create(req.body, (err, data) => {
-        if (err) {
-          res.json({
-            status: 'fail',
-            msg: req.body.dname
-          })
-        } else {
-          res.json({
-            status: 'success',
-            msg: req.body.dname
-          })
-        }
-      })
-    } else {
-      res.json({
-        status: 'error',
-        msg: 'unauthorized'
-      })
-    }
+  // user login & permission judgment
+  if (req.session.userinfo.uauth == 'admin') {
+    // create dept
+    Dept.create(req.body, (err, data) => {
+      if (err) {
+        res.json({
+          status: 'fail',
+          msg: req.body.dname
+        })
+      } else {
+        res.json({
+          status: 'success',
+          msg: req.body.dname
+        })
+      }
+    })
   } else {
     res.json({
       status: 'error',
@@ -71,30 +55,22 @@ router.post('/dept/add', (req, res) => {
 
 // dept del interface
 router.post('/dept/del', (req, res) => {
-  // user login judgment
-  if (req.session.userinfo) {
-    // user permission judgment
-    if (req.session.userinfo.uauth == 'admin') {
-      // delete dept
-      Dept.findOneAndDelete(req.body)
-        .then(dept => {
-          res.json({
-            status: 'success',
-            msg: req.body.dname
-          })
+  // user login & permission judgment
+  if (req.session.userinfo.uauth == 'admin') {
+    // delete dept
+    Dept.findOneAndDelete(req.body)
+      .then(dept => {
+        res.json({
+          status: 'success',
+          msg: req.body.dname
         })
-        .catch(err => {
-          res.json({
-            status: 'fail',
-            msg: req.body.dname
-          })
-        })
-    } else {
-      res.json({
-        status: 'error',
-        msg: 'unauthorized'
       })
-    }
+      .catch(err => {
+        res.json({
+          status: 'fail',
+          msg: req.body.dname
+        })
+      })
   } else {
     res.json({
       status: 'error',
@@ -105,30 +81,22 @@ router.post('/dept/del', (req, res) => {
 
 // dept mod interface
 router.post('/dept/mod', (req, res) => {
-  // user login judgment
-  if (req.session.userinfo) {
-    // user permission judgment
-    if (req.session.userinfo.uauth == 'admin') {
-      // modify dept
-      Dept.findOneAndUpdate({ dname: req.body.dname }, { $set: { ddesc: req.body.ddesc } }, { new: true })
-        .then(dept => {
-          res.json({
-            status: 'success',
-            msg: req.body.dname
-          })
+  // user login & permission judgment
+  if (req.session.userinfo.uauth == 'admin') {
+    // modify dept
+    Dept.findOneAndUpdate({ dname: req.body.dname }, { $set: { ddesc: req.body.ddesc } }, { new: true })
+      .then(dept => {
+        res.json({
+          status: 'success',
+          msg: req.body.dname
         })
-        .catch(err => {
-          res.json({
-            status: 'fail',
-            msg: req.body.dname
-          })
-        })
-    } else {
-      res.json({
-        status: 'error',
-        msg: 'unauthorized'
       })
-    }
+      .catch(err => {
+        res.json({
+          status: 'fail',
+          msg: req.body.dname
+        })
+      })
   } else {
     res.json({
       status: 'error',
