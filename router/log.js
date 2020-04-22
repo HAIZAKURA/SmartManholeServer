4b455e20e2bb56e8756f8fe65f2a64f69f15a7fd
HAIZAKURA <hhbilly99@gmail.com>
2020-04-22 11:30:17 +0800

    Create log.js

---
+++ router/log.js
@@ -0,0 +1,84 @@
+// router/log.js
+// log router file
+
+const express = require('express')
+const router = express.Router()
+
+function getNowDate() {
+  let date = new Date()
+  let year = date.getFullYear()
+  let month = date.getMonth() + 1
+  let day = date.getDate()
+  let hour = date.getHours()
+  let minute = date.getMinutes()
+  let second = date.getSeconds()
+  let formatDate = year + '-' + month + '-' + day + '-' + hour + '-' + minute + '-' + second
+  return formatDate
+}
+
+// import log schema
+const Log = require('../schema/log')
+
+// get logs interface
+router.post('/log', (req, res) => {
+  // user login judgment
+  if (req.session.userinfo) {
+    // get logs
+    Log.find({ ldep: req.session.udept }, { _id: 0 }).gte('ldat', req.body.ldatbeg).lte('ldat', req.body.ldatend)
+      .sort({ ldat: 1 })
+      .then(loglist => {
+        res.json({
+          status: 'success',
+          log: loglist
+        })
+      })
+      .catch(err => {
+        res.json({
+          status: 'fail',
+          msg: req.session.udept
+        })
+      })
+  } else {
+    res.json({
+      status: 'error',
+      msg: 'unauthorized'
+    })
+  }
+})
+
+// create logs interface
+router.post('/log/add', (req, res) => {
+  // user login judgment
+  if (req.session.userinfo) {
+    // add data
+    addData = {
+      'ldev': req.body.ldev,
+      'ldep': req.session.userinfo.udept,
+      'lopt': req.body.lopt,
+      'lusr': req.session.userinfo.uname,
+      'ldat': getNowDate()
+    }
+    // create logs
+    Log.create(addData, (err, data) => {
+      if (err) {
+        res.json({
+          status: 'fail',
+          msg: req.body.ldev
+        })
+      } else {
+        res.json({
+          status: 'success',
+          msg: req.body.ldev
+        })
+      }
+    })
+  } else {
+    res.json({
+      status: 'error',
+      msg: 'unauthorized'
+    })
+  }
+})
+
+// export log router
+module.exports = router
