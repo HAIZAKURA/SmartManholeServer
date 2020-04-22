2551e5e5658608fff7331c07bfdc29cf47696ef5
HAIZAKURA <hhbilly99@gmail.com>
2020-04-22 11:30:18 +0800

    Create device.js

---
+++ router/device.js
@@ -0,0 +1,11 @@
+// router/device.js
+// device router file
+
+const express = require('express')
+const router = express.Router()
+
+// import device schema
+const Device = require('../schema/device')
+
+// export device router
+module.exports = router
